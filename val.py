5a375f870d4d69115e1c2e250aad6b40e1696375
Andrew <131379952+Andrew-Dylan@users.noreply.github.com>
2026-05-29 08:59:47 +0800

    Modify model loading and add result output
    Updated model initialization and added result printing.

--- val.py
+++ val.py
@@ -1,14 +1,18 @@
 import warnings
 warnings.filterwarnings('ignore')
+
 from ultralytics import RTDETR
 
 if __name__ == '__main__':
-    model = RTDETR('runs/train/exp/weights/best.pt')
-    model.val(data='dataset/data.yaml',
-              split='test',
-              imgsz=640,
-              batch=4,
-            # save_json=True,
-              project='runs/val',
-              name='exp',
-              )
+    model_path = 'your_model.pt' 
+    model = RTDETR(model_path)
+    result = model.val(
+        data='dataset/data.yaml',
+        split='test',
+        imgsz=640,
+        batch=4,
+        device=0,
+        project='runs/val',
+        name='exp',
+    )
+    print(result)
