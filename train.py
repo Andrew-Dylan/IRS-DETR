c04e4b734d23861e8d5d71d2f212c359829d77ac
Andrew <131379952+Andrew-Dylan@users.noreply.github.com>
2026-05-29 09:00:15 +0800

    Update model configuration and training dataset

--- train.py
+++ train.py
@@ -1,18 +1,17 @@
-import warnings
+import warnings, os
+
 warnings.filterwarnings('ignore')
 from ultralytics import RTDETR
 
 if __name__ == '__main__':
-    model = RTDETR('ultralytics/cfg/models/rt-detr/rtdetr-r18.yaml')
-    # model.load('') # loading pretrain weights
-    model.train(data='dataset/data.yaml',
+    model = RTDETR('your_model.yaml') # 模型配置文件路径
+    model.train(data='dataset/ISDD.yaml',
                 cache=False,
                 imgsz=640,
                 epochs=300,
                 batch=4,
                 workers=4,
-                device='0',
-                # resume='', # last.pt path
+                patience=0,
                 project='runs/train',
                 name='exp',
                 )
