16deef9539bb215f7e7fbb212017c7d4509d40aa
Andrew <131379952+Andrew-Dylan@users.noreply.github.com>
2026-05-29 09:34:33 +0800

    Update comment for model path in detect.py

--- detect.py
+++ detect.py
@@ -3,7 +3,7 @@ warnings.filterwarnings('ignore')
 from ultralytics import RTDETR
 
 if __name__ == '__main__':
-    model = RTDETR('your_model.pt') # select your model.pt path
+    model = RTDETR('your_model.pt') # select your model path
     model.predict(source='dataset/images/test',
                   conf=0.25,
                   project='runs/detect',
